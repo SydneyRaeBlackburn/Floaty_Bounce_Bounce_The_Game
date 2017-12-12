656f8c13577cdba55d78bbf3a565bba9be33310b
Sydney Blackburn <Sydney@Sydneys-MacBook-Air.local>
2017-12-12 15:58:40 -0800

    Added background music and AudioManager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour 
+{
+	// Code from unity tutorial about creating an audio and sound manager
+	// implements singleton
+	// sound effects from Raiden's 2D snake tutorial
+
+	public AudioSource efxSource;                   //Drag a reference to the audio source which will play the sound effects.
+	public AudioSource musicSource;                 //Drag a reference to the audio source which will play the music.
+	public static AudioManager instance = null;     //Allows other scripts to call functions from SoundManager.             
+
+	void Awake ()
+	{
+		//Check if there is already an instance of SoundManager
+		if (instance == null)
+			//if not, set it to this.
+			instance = this;
+		//If instance already exists:
+		else if (instance != this)
+			//Destroy this, this enforces our singleton pattern so there can only be one instance of SoundManager.
+			Destroy (gameObject);
+
+		//Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+		DontDestroyOnLoad (gameObject);
+	}
+
+
+	//Used to play single sound clips.
+	public void PlaySingle(AudioClip clip)
+	{
+		// Use AudioManager.instance.PlaySingle (effect); in other scripts to play sound effects
+
+		//Set the clip of our efxSource audio source to the clip passed in as a parameter.
+		efxSource.clip = clip;
+
+		//Play the clip.
+		efxSource.Play ();
+	}
+}
+
