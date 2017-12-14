0369e096708eed5167640f0a4f228dd061388883
Chris Schoonover <schoonc@gmail.com>
2017-12-14 15:49:58 -0800

    Splash scene fixed

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+
+    public Image splashImage;
+    public string loadScene;
+
+    IEnumerator Start()
+    {
+        splashImage.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn();
+        yield return new WaitForSeconds(3.0f);
+        SceneManager.LoadScene(loadScene);
+    }
+
+    void FadeIn()
+    {
+        splashImage.CrossFadeAlpha(1.0f, 2.0f, false);
+    }
+}
